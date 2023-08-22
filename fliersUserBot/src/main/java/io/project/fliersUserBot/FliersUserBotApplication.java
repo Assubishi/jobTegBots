a67975c5f702eb3811557273fc20ba27e13e2c9b
Assubishi <Khamiyev02@gmail.com>
2023-08-22 11:00:36 +0600

    aaaaaa

---
+++ fliersUserBot/src/main/java/io/project/fliersUserBot/FliersUserBotApplication.java
@@ -0,0 +1,13 @@
+package io.project.fliersUserBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FliersUserBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FliersUserBotApplication.class, args);
+	}
+
+}
